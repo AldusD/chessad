68e9e076e10e02657254403d1f08f51fba45abf4
aldusd <aldusdanielfsm@gmail.com>
2023-02-26 12:18:51 -0300

    feat: setup tests

--- back/chess-ad-server/src/app.ts
+++ back/chess-ad-server/src/app.ts
@@ -3,7 +3,7 @@ import "express-async-errors";
 import express, { Express } from "express";
 import cors from "cors";
 
-import { loadEnv, connectDb, disconnectDB, prisma } from "@/config";
+import { loadEnv, connectDb, disconnectDB, prisma } from "./config";
 import { hRouter } from "./routers";
 loadEnv();
 
@@ -12,30 +12,7 @@ app
   .use(cors())
   .use(express.json())
   .get("/health", (_req, res) => res.send("1.e4!"))
-  .use("/h", hRouter)
-  .get("/users", async (req, res) => {
-    try { 
-      const users = await prisma.user.findMany();
-      res.status(200).send(users)
-    } catch (error) {
-      console.log(error)
-      res.status(400).send(error)
-    }
-  }).post("/users", async (req, res) => {
-    try { 
-      const users = await prisma.user.create({
-        data: {
-          username: 'allduss',
-          email: 'aaa@aaa.com',
-          profilePicture: 'aa'
-        }
-      });
-      res.status(201).send(users)
-    } catch (error) {
-      console.log(error)
-      res.status(400).send(error)
-    }
-  })
+  .use("/h", hRouter);
 
 export function init(): Promise<Express> {
   connectDb();
---
+++ back/chess-ad-server/tests/integration/users.test.ts
@@ -0,0 +1,115 @@
+import faker from "@faker-js/faker";
+import supertest from "supertest";
+import httpStatus from "http-status";
+
+import app, { init } from "../../src/app";
+import { cleanDb } from "../helpers";
+import { createUser } from "../factories";
+
+beforeAll(async () => {
+  await init();
+  await cleanDb();
+});
+
+const server = supertest(app);
+
+describe("POST /auth/sign-in", () => {
+  it("should respond with status 422 when body is not given", async () => {
+    const response = await server.post("/auth/sign-in");
+    expect(response.status).toBe(httpStatus.UNPROCESSABLE_ENTITY);
+  });
+
+  it("should respond with status 422 when body is not valid", async () => {
+    const invalidBody = { [faker.lorem.word()]: faker.lorem.word() };
+
+    const response = await server.post("/auth/sign-in").send(invalidBody);
+
+    expect(response.status).toBe(httpStatus.UNPROCESSABLE_ENTITY);
+  });
+
+  describe("when body is valid", () => {
+    const generateValidBody = () => ({
+      email: faker.internet.email(),
+      password: faker.internet.password(6),
+    });
+
+    it("should respond with status 401 if there is no user for given email", async () => {
+      const body = generateValidBody();
+
+      const response = await server.post("/auth/sign-in").send(body);
+
+      expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it("should respond with status 401 if there is a user for given email but password is not correct", async () => {
+      const body = generateValidBody();
+      await createUser(body);
+
+      const response = await server.post("/auth/sign-in").send({
+        email: body.email,
+        password: faker.lorem.word()
+      });
+
+      expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    describe("when credentials are valid", () => {
+      it("should respond with status 200", async () => {
+        const body = generateValidBody();
+        await createUser(body);
+
+        const response = await server.post("/auth/sign-in").send(body);
+
+        expect(response.status).toBe(httpStatus.OK);
+        expect(response.body.token).toBeDefined();
+      });
+    });
+  });
+});
+
+describe("POST /auth/sign-up", () => {
+  it("should respond with status 422 when body is not given", async () => {
+    const response = await server.post("/auth/sign-up");
+    expect(response.status).toBe(httpStatus.UNPROCESSABLE_ENTITY);
+  });
+
+  it("should respond with status 422 when body is not valid", async () => {
+    const invalidBody = { [faker.lorem.word()]: faker.lorem.word() };
+    const response = await server.post("/auth/sign-up").send(invalidBody);
+    expect(response.status).toBe(httpStatus.UNPROCESSABLE_ENTITY);
+  });
+
+  describe("when body is valid", () => {
+    const generateValidBody = () => ({
+      username: faker.lorem.word(),
+      email: faker.internet.email(),
+      password: faker.internet.password(6),
+    });
+
+    it("should respond with status 409 if there is already an user for given email", async () => {
+      const body = generateValidBody();
+      createUser({ email: body.email });
+
+      const response = await server.post("/auth/sign-up").send(body);
+
+      expect(response.status).toBe(httpStatus.CONFLICT);
+    });
+
+    it("should respond with status 409 if username is already in use", async () => {
+      const body = generateValidBody();
+      createUser({ email: faker.internet.email(), username: body.username });
+
+      const response = await server.post("/auth/sign-up").send(body);
+
+      expect(response.status).toBe(httpStatus.CONFLICT);
+    });
+
+    it("should respond with status 201", async () => {
+      const body = generateValidBody();
+
+      const response = await server.post("/auth/sign-in").send(body);
+
+      expect(response.status).toBe(httpStatus.CREATED);
+    });
+  });
+})
